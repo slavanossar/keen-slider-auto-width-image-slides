52a74f50e23326050a1bf9d37a1c5ea4a4a730a7
Slava Nossar <slava.nossar@gmail.com>
2022-09-20 18:20:52 +1000

    add nav and other page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div class="p-24">
+    <div class="mb-4 flex space-x-4 border-b border-black pb-4">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/other">Other page</NuxtLink>
+    </div>
     <slot />
   </div>
 </template>
---
+++ pages/other.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex h-screen flex-col items-center justify-center">
+    <span>Nothing here...</span>
+    <NuxtLink to="/" class="underline">Back to sliders</NuxtLink>
+  </div>
+</template>
